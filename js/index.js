2791b4f0ede217d124afc3e1ca7ca323fd71dfba
Gsgzs <562180496@qq.com>
2020-04-21 12:08:10 +0800

    2020.4.21

--- js/index.js
+++ js/index.js
@@ -1,14 +1,13 @@
 $(function () {
-
-    // 当前位置F5之后
-
-
-
-
-
-
-
     // 【页头部分】
+    (function () {
+        let v = $(window).scrollTop();
+        if (v != 0) {
+            $('.wrap-head').stop().css({
+                top: -72
+            })
+        }
+    })()
     // 移入移出导航
     $('.head-nav').hover(function () {
             $('.nav-hook').show();
@@ -18,19 +17,22 @@ $(function () {
             $('.head-con .lang>a').addClass('a_show');
             $('.head-show').stop().animate({
                 opacity: 1,
-                height: 446
+                height: 516
             }, 300)
         },
         function () {
+            let v = $(window).scrollTop();
             $('.nav-hook').hide();
-            $('.head-con .lang').removeClass('lang_show');
-            $('.head-con .lang>a').removeClass('a_show');
             $('.head-show').stop().animate({
                 opacity: 0,
                 height: 0
             }, 160, function () {
-                $('.logo a').removeClass('logo_show');
-                $('.head-nav .nav-item>.title').removeClass('title_hover');
+                if (v == 0) {
+                    $('.logo a').removeClass('logo_show');
+                    $('.head-nav .nav-item>.title').removeClass('title_hover');
+                    $('.head-con .lang').removeClass('lang_show');
+                    $('.head-con .lang>a').removeClass('a_show');
+                }
             })
         })
 
@@ -99,8 +101,67 @@ $(function () {
     }, 500)
 
 
+    // connect连接tab栏
+    $('.connect-tab>.tab-list li').mouseenter(function () {
+        $(this).addClass('current').siblings('li').removeClass('current');
+        // tab内容
+        let index = $(this).index();
+        $('.connect-con>.con').eq(index).stop(0).fadeIn().siblings('.con').stop().fadeOut(0);
+    })
+
+    // rspbve轮播图-opacity
+    let rbNum = 0;
+    // 右按钮
+    $('.rspbve-area>.slide-area .icon_arrow_r').click(function () {
+        rbNum++;
+        if (rbNum == $('.rspbve-area>.slide-area li').length) rbNum = 0;
+        $('.rspbve-area>.slide-area li').eq(rbNum).stop().animate({
+            opacity: 1
+        }, 300).siblings('li').stop().animate({
+            opacity: 0
+        }, 300)
+    })
+    // 左按钮
+    $('.rspbve-area>.slide-area .icon_arrow_l').click(function () {
+        rbNum--;
+        if (rbNum < 0) rbNum = $('.rspbve-area>.slide-area li').length - 1;
+        $('.rspbve-area>.slide-area li').eq(rbNum).stop().animate({
+            opacity: 1
+        }, 300).siblings('li').stop().animate({
+            opacity: 0
+        }, 300)
+    })
+    // 自动播放
+    let rbTimer = setInterval(function () {
+        $('.rspbve-area>.slide-area .icon_arrow_r').click();
+    }, 4000)
 
 
+    // tasdvp轮播图-opacity
+    let tdNum = 0;
+    // 自动播放
+    function tasdvp_slide(i) {
+        tdNum++;
+        if (tdNum == $('.tasdvp-area>.slide-area>.slide-list li').length) tdNum = 0;
+        $('.tasdvp-area>.slide-area>.slide-list li').eq(tdNum).stop().animate({
+            opacity: 1
+        }, 300).siblings('li').stop().animate({
+            opacity: 0
+        }, 300)
+        // 删 添 删 添 [效果不一般]
+        $('.tasdvp-area>.slide-area>.slide-tab li').removeClass('active');
+        $('.tasdvp-area>.slide-area>.slide-tab li').eq(tdNum).addClass('active');
+        $('.tasdvp-area>.slide-area>.slide-tab li').removeClass('active');
+        $('.tasdvp-area>.slide-area>.slide-tab li').eq(tdNum).addClass('active');
+    }
+    let tdTimer = setInterval(tasdvp_slide, 5000);
+    // 点击小点
+    $('.tasdvp-area>.slide-area>.slide-tab li').click(function () {
+        clearInterval(tdTimer)
+        tdNum = $(this).index() - 1;
+        tasdvp_slide();
+        tdTimer = setInterval(tasdvp_slide, 4000);
+    })
 
 
 })
