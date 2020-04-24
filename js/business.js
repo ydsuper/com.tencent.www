302d994da5546afed9bdd8c01e8794c779427e54
Gsgzs <562180496@qq.com>
2020-04-24 18:55:57 +0800

    2020.4.25

--- js/business.js
+++ js/business.js
@@ -1,15 +1,17 @@
 $(function () {
     // 监听滚动事件
     let windowTop = 0;
-    var showHeadState = true;
+    let showHeadState = true;
     $(window).scroll(function () {
         let v = $(this).scrollTop();
         if (v > windowTop) {
+            $('.head-nav .nav-item').hide();
             $('.wrap-head').stop().css({
                 top: -72
             })
             windowTop = v;
         } else {
+            $('.head-nav .nav-item').show();
             if (showHeadState) {
                 $('.wrap-head').stop().css({
                     top: 0
@@ -30,6 +32,13 @@ $(function () {
             $('.biz-nav-roll').removeClass('biz-nav-fixed');
         }
     })
+    // resize变化
+    $(window).resize(function () {
+        $('.biz-nav .biz-list').css({
+            paddingLeft: 16
+        })
+    })
+
 
 
     // app弹窗
@@ -69,14 +78,19 @@ $(function () {
     })
 
 
+    // [business_to-b]
+    // 智慧产业tab
+    $('.brand-item-wdid .con-bottom>.tab-box li').mouseenter(function () {
+        let index = $(this).index();
+        $(this).addClass('active-t').siblings('li').removeClass('active-t');
+        $('.brand-item-wdid .con-bottom>.pic-box li').eq(index).addClass('active-p').siblings('li').removeClass('active-p')
+    })
 
 
 
 
 
 
-
-    
 
 
 
