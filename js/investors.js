e338f1e71e76366e22adb828a3eed6268e5c369c
Gsgzs <562180496@qq.com>
2020-04-26 17:28:35 +0800

    2020.4.27

---
+++ js/investors.js
@@ -0,0 +1,29 @@
+$(function () {
+    // 监听滚动事件
+    let windowTop = 0;
+    let showHeadState = true;
+    $(window).scroll(function () {
+        let v = $(this).scrollTop();
+        if (v > windowTop) {
+            $('.head-nav .nav-item').hide();
+            $('.wrap-head').stop().css({
+                top: -72
+            })
+            windowTop = v;
+        } else {
+            $('.head-nav .nav-item').show();
+            if (showHeadState) {
+                $('.wrap-head').stop().css({
+                    top: 0
+                })
+            }
+            windowTop = v;
+        }
+    })
+
+
+
+
+    
+
+})
