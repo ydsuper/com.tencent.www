9971f8387faa7c78b3daa8273784243c37f1721e
Gsgzs <562180496@qq.com>
2020-04-27 21:15:57 +0800

    2020.4.28

--- js/investors.js
+++ js/investors.js
@@ -21,6 +21,38 @@ $(function () {
         }
     })
 
+    // 投资者日历[tab栏] + [下拉列表]
+    // tab
+    $('.ivcal-box .tab-list>li').click(function () {
+        $(this).addClass('active-tab').siblings('li').removeClass('active-tab');
+        // tab-con
+        let index = $(this).index();
+        $('.ivcal-box .tab-con>li').eq(index).addClass('active-con').siblings('li').removeClass('active-con');
+    })
+    // 下拉
+    $('.ivcal-box .tab-con>li .item').click(function () {
+        if ($(this).hasClass('up')) {
+            $(this).addClass('down');
+            let $this = $(this);
+            setTimeout(function () {
+                $this.removeClass('up');
+                $this.removeClass('down');
+            }, 450);
+        } else {
+            $(this).toggleClass('up');
+        }
+        $(this).find('.detail').stop().slideToggle();
+    })
+
+
+
+
+
+
+
+
+
+
 
 
 
